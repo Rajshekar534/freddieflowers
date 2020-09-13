4585d76a88618d36c409190fc79bf763d7399007
Rajasekhar <rajshekar.kota@gmail.com>
2020-09-13 12:01:52 +0530

    uploaded freddieflowers

--- src/Services/Subscription/GetScheduledOrders.php
+++ src/Services/Subscription/GetScheduledOrders.php
@@ -3,6 +3,7 @@
 namespace App\Services\Subscription;
 
 use App\Entities\Subscription;
+use App\Entities\ScheduledOrder;
 
 class GetScheduledOrders
 {
@@ -16,6 +17,23 @@ class GetScheduledOrders
      */
     public function handle(Subscription $subscription, $forNumberOfWeeks = 6)
     {
-        //
+        if ($subscription->getStatus() === Subscription::STATUSES_ALLOWED[Subscription::STATUS_ACTIVE]) {
+            $scheduledOrders = [];
+            $nextDeliveryDate = $subscription->getNextDeliveryDate();
+            $deliveryDate = $nextDeliveryDate->copy();
+            $isInterval = $subscription->getPlan() === Subscription::PLANS_ALLOWED[Subscription::PLAN_WEEKLY] ? TRUE : FALSE;
+            $interval = true;
+            for ($i = 0; $i < $forNumberOfWeeks; $i++) {
+                $scheduledOrder = new ScheduledOrder($deliveryDate, $interval);
+                $scheduledOrder->setHoliday(!$interval);
+                $scheduledOrder->setOptIn($interval);
+                $scheduledOrders[] = $scheduledOrder;
+                $deliveryDate = $deliveryDate->copy()->addWeek();
+                $interval = $isInterval ? $interval : !$interval;
+            }
+            return $scheduledOrders;
+        } else {
+            return [];
+        }
     }
 }
