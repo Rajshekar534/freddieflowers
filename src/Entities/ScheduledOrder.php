8b41e104a3942375620e603f39416eda527ec0f6
Rajasekhar Reddy <47551590+Rajshekar534@users.noreply.github.com>
2020-09-13 11:22:32 +0530

    Add files via upload

--- src/Entities/ScheduledOrder.php
+++ src/Entities/ScheduledOrder.php
@@ -38,11 +38,72 @@ class ScheduledOrder
      * ScheduledOrder constructor.
      *
      * @param \Carbon\Carbon     $deliveryDate
-     * @param \App\Entities\bool $isInterval
+     * @param bool $isInterval
      */
     public function __construct(Carbon $deliveryDate, bool $isInterval)
     {
         $this->deliveryDate = $deliveryDate;
         $this->interval     = $isInterval;
     }
+    
+    /**
+     * Is used to check scheduled order part of the subscriptions normal plan cycle.
+     * @return bool
+     */
+    public function isInterval(): bool
+    {
+        return $this->interval;
+    }
+    
+    /**
+     * To set a delivery date as holiday
+     * @param bool $holiday
+     * @return self 
+     */
+    public function setHoliday(bool $holiday)
+    {
+        $this->holiday = $this->isInterval() ? $holiday : false;
+
+        return $this;
+    }
+    
+    /**
+     * To check the delivery date is a holiday
+     * @return bool
+     */
+    public function isHoliday(): bool
+    {
+        
+        return $this->holiday;
+    }
+    
+    /**
+     * To get delivery date
+     * @return \Carbon\Carbon
+     */
+    public function getDeliveryDate(): \Carbon\Carbon
+    {
+        return $this->deliveryDate;
+    }
+    
+    /**
+     * To scheduled order an opt in order that is not part of the normal subscription's plan cycle.
+     * @return self
+     */
+    public function setOptIn(bool $isOptIn)
+    {
+        $this->optIn = $this->isInterval() ? false : $isOptIn;
+        
+        return $this;
+    }
+    
+    /**
+     * To check scheduled order an opt in order that is not part of the normal subscription's plan cycle.
+     * @return bool
+     */
+    public function isOptIn(): bool
+    {
+        return $this->optIn;
+    }
+    
 }
